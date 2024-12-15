7b9687627e1ba7948eed2b76391705dce2927349
ATOMIC-HEART-777 <denis1977chupin@gmail.com>
2024-12-15 20:47:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+total_tasks = 12
+print(12)
+number_of_hours_spent = 1.5
+print(1.5)
+course_name = "Python"
+print("Python")
+average_execution_time = 0.125
+print(0.125)
+print(course_name,total_tasks,number_of_hours_spent,average_execution_time)
+
+
+
+
